2d95e7824160e163084728d279996afc174c868f
cnlucke <cristy.lucke@gmail.com>
2018-03-25 16:01:02 -0400

    updating api results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user
+      render json: @user, serializer: UsersSerializer
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      # params.permit(:user, :username, :password)
+      params.require(:user).permit(:username, :password)
     end
 end
